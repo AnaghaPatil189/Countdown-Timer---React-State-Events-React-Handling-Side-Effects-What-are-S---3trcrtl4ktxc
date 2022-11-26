73cd80d10fd41955301d88bbf7df94ecb4a58bfd
AnaghaPatil189 <109735323+AnaghaPatil189@users.noreply.github.com>
2022-11-26 11:52:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,42 @@
-import React, { useState } from "react";
-
+import React, { Component, useState, useEffect } from "react";
 import "../styles/App.css";
 
-const data = {
-  2018: ["Avengers 1", "Now you see me", "Fast and Furious 7"],
-  2019: ["Avengers 2", "Now you see me 2", "Fast and Furious 8"],
-  2020: [
-    "Final Avengers Movie(Iron man dies)",
-    "Now you finally used Lenskart",
-    "Covid Came",
-  ],
-  2021: ["Covid Returns"],
-  2022: ["Adventures of Saiman", "Adventures of Shaktiman"],
-};
 const App = () => {
-  const [year, setYear] = useState("");
-  const changeYear = (e) => {
-    console.log(e.target.value);
-    setYear(e.target.value);
+  let timerId = "";
+  let [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    if (timeLeft > 0) {
+      timerId = setTimeout(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+    }
+  }, [timeLeft]);
+
+  const countInReverse = (e) => {
+    if (e.keyCode === 13) {
+      clearTimeout(timerId);
+      let timeCountValue = document.getElementById("timeCount").value;
+      if (!isNaN(timeCountValue)) {
+        setTimeLeft(parseInt(timeCountValue));
+      } else {
+        if (timerId !== "") {
+          clearTimeout(timerId);
+        }
+        setTimeLeft(0);
+      }
+    }
   };
+
   return (
-    <div id="main">
-      <select name="" id="" onChange={changeYear}>
-        <option value={null}></option>
-        {Object.keys(data).map((key) => {
-          return (
-            <option key={key} value={key}>
-              {key}
-            </option>
-          );
-        })}
-      </select>
-      <div id="selected-year">
-        {year ? `Selected year-${year}` : "No year selected"}
+    <div className="main">
+      <div id="whole-center">
+        <h1>
+          Reverse countdown for
+          <input id="timeCount" onKeyDown={countInReverse} /> sec.
+        </h1>
       </div>
-      <ul>
-        {year &&
-          data[year].map((movie) => {
-            return <li key={movie}>{movie}</li>;
-          })}
-      </ul>
+      <div id="current-time">{timeLeft}</div>
     </div>
   );
 };
