4461ea4bc7875711618906cb08251b4cedd140f0
AnaghaPatil189 <109735323+AnaghaPatil189@users.noreply.github.com>
2022-11-26 11:50:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,47 @@
-import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import React, { useState } from "react";
 
+import "../styles/App.css";
+
+const data = {
+  2018: ["Avengers 1", "Now you see me", "Fast and Furious 7"],
+  2019: ["Avengers 2", "Now you see me 2", "Fast and Furious 8"],
+  2020: [
+    "Final Avengers Movie(Iron man dies)",
+    "Now you finally used Lenskart",
+    "Covid Came",
+  ],
+  2021: ["Covid Returns"],
+  2022: ["Adventures of Saiman", "Adventures of Shaktiman"],
+};
 const App = () => {
-  // write your code here 
-
+  const [year, setYear] = useState("");
+  const changeYear = (e) => {
+    console.log(e.target.value);
+    setYear(e.target.value);
+  };
   return (
-    <div className="wrapper">
-      <div id="whole-center">
-        <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
-        </h1>
+    <div id="main">
+      <select name="" id="" onChange={changeYear}>
+        <option value={null}></option>
+        {Object.keys(data).map((key) => {
+          return (
+            <option key={key} value={key}>
+              {key}
+            </option>
+          );
+        })}
+      </select>
+      <div id="selected-year">
+        {year ? `Selected year-${year}` : "No year selected"}
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <ul>
+        {year &&
+          data[year].map((movie) => {
+            return <li key={movie}>{movie}</li>;
+          })}
+      </ul>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
